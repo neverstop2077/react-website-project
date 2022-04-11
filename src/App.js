f291377866452a1202394c0f937103372bebb050
neverstop2077 <quocduy060220@gmail.com>
2022-04-11 17:01:04 +0700

    fix sth

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path='/products' element={<Products />} />
           <Route path='/sign-up' element={<SignUp />} />
         </Routes>
+        <Footer />
       </Router>
     </>
   );
